4a9b35d1234848c63e4e61e9d9de0baa91cf1466
melanxoluk <melancholyprogrammer@gmail.com>
2019-07-07 17:31:00 +0300

    jsx styling

--- pages/post.js
+++ pages/post.js
@@ -1,11 +1,35 @@
 import {withRouter} from "next/router";
 import Layout from "../components/MyLayout";
+import Markdown from "react-markdown"
+import fetch from "isomorphic-unfetch"
 
 const Post = props => (
   <Layout>
     <h1>{props.show.name}</h1>
-    <p>{props.show.summary.replace(/<[/]?p>]/g, '')}</p>
+    <div className="markdown">
+      <Markdown source={props.show.summary.replace(/<[/]?p>]/g, '')}/>
+    </div>
     <img src={props.show.image.medium}/>
+    <style jsx global>{`
+      .markdown {
+        font-family: 'Arial';
+      }
+      
+      .markdown a {
+        text-decoration: none;
+        color: blue;
+      }
+      
+      .markdown a:hover {
+        opacity: 0.6;
+      }
+      
+      .markdown h3 {
+        margin: 0;
+        padding: 0;
+        text-transform: uppercase;
+      }
+    `}</style>
   </Layout>
 );
 
