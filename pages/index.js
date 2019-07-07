b3b8f33a3210531a667da190ab4facd7a5b0ba77
melanxoluk <melancholyprogrammer@gmail.com>
2019-07-07 16:42:25 +0300

    dynamic pages

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,23 @@
+import Link from "next/link"
 import Layout from "../components/MyLayout";
 
-const Index = () => (
-  <Layout>
-    <p>Hello Nuxt.js</p>
-  </Layout>
+const PostLink = props => (
+  <li>
+    <Link href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
 );
 
-export default Index;
+export default function Blog() {
+  return (
+    <Layout>
+      <h1>My Blog</h1>
+      <ul>
+        <PostLink title="Hello Nuxt.js"/>
+        <PostLink title="Learn Nuxt.js"/>
+        <PostLink title="Deploy"/>
+      </ul>
+    </Layout>
+  );
+}
