a3c4d783285ccc4f6368613bd6fad99b5b37713f
melanxoluk <melancholyprogrammer@gmail.com>
2019-07-07 17:17:27 +0300

    fetching data

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,31 @@
 import Link from "next/link"
 import Layout from "../components/MyLayout";
+import fetch from "isomorphic-unfetch"
 
-const PostLink = props => (
-  <li>
-    <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
-      <a>{props.title}</a>
-    </Link>
-  </li>
+const Index = props => (
+  <Layout>
+    <h1>Batman TV Shows</h1>
+    <ul>
+      {props.shows.map(show => (
+        <li key={show.id}>
+          <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
+            <a>{show.name}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </Layout>
 );
 
-export default function Blog() {
-  return (
-    <Layout>
-      <h1>My Blog</h1>
-      <ul>
-        <PostLink id="hello-nextjs" title="Hello Nuxt.js"/>
-        <PostLink id="learn-nextjs" title="Learn Nuxt.js"/>
-        <PostLink id="deploy-nextjs" title="Deploy"/>
-      </ul>
-    </Layout>
-  );
-}
+Index.getInitialProps = async function () {
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+  const data = await res.json();
+
+  console.info(`Show data fetched. Count: ${data.length}`)
+
+  return {
+    shows: data.map(entry => entry.show)
+  };
+};
+
+export default Index;
